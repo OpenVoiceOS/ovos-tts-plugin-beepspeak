ac641218b8f8b87c3dcf7df585aab362ab9b5974
NeonJarbas <59943014+NeonJarbas@users.noreply.github.com>
2022-08-02 18:30:50 +0100

    add "offline" metadata key (#2)
    Co-authored-by: jarbasai <jarbasai@mailfence.com>

--- ovos_tts_plugin_beepspeak/__init__.py
+++ ovos_tts_plugin_beepspeak/__init__.py
@@ -60,6 +60,6 @@ class BeepSpeakValidator(TTSValidator):
 
 BeepSpeakTTSPluginConfig = {
     lang: [
-        {"voice": "r2d2", "gender": "neutral", "display_name": "R2D2"}
-    ] for lang in ["en", "es", "pt", "it", "fr", "de", "nl", "uk", "ru"]
+        {"voice": "r2d2", "gender": "neutral", "display_name": "R2D2", "offline": True}
+    ] for lang in ["en"]  # TODO, do we want to spam this in every lang? add a special lang code for "any" ?
 }
